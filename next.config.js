45b1a5dc378cd0c5137b3afa2639150960dd0b4d
ovdncids <ovdncids@nate.com>
2023-11-19 13:42:17 +0900

    ESLint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  eslint: {
+    dirs: ["/"]
+  }
 }
 
 module.exports = nextConfig
