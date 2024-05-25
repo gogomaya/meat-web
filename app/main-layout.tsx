7519f1eb269a54194243587a56e51d67e2d07773
wwwaloha <53545071+wwwalohacampus@users.noreply.github.com>
2024-05-25 22:37:44 +0900

    fixed -> sticky (임의 mt py  줄필요 없음)

--- app/main-layout.tsx
+++ app/main-layout.tsx
@@ -49,7 +49,7 @@ const MainLayout = ({
     <div className="mx-auto">
       <header
         id="header"
-        className="fixed top-0 z-20 bg-cover bg-center bg-opacity-90 w-full flex justify-center items-center px-4 py-4 md:py-8 md:px-12 text-white transition-opacity duration-300"
+        className="top-0 z-20 bg-cover bg-center bg-opacity-90 w-full flex justify-center items-center px-4 py-4 md:py-8 md:px-12 text-white transition-opacity duration-300"
         style={{backgroundImage: "url('/images/Bg.png')", backgroundPosition: "center calc(50% - 235px)", opacity: headerOpacity}}
       >
         <MainMobileMenu />
@@ -73,7 +73,7 @@ const MainLayout = ({
           </Badge>
         </Link>
       </header>
-      <main className="pt-8 md:pt-16">{children}</main>
+      <main>{children}</main>
       <CsIcon />
       <MainBottom />
     </div>
