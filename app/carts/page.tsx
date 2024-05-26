24e7f34b1c0b37dc98236e0f9764d6fcbf7d6c9b
gogomaya <ssdd03580@gmail.com>
2024-05-26 12:00:03 +0900

    Add cart redesign

--- app/carts/page.tsx
+++ app/carts/page.tsx
@@ -6,7 +6,20 @@ const Cart = async () => {
   const {user} = await loginCheck(false)
   return (
     <MainLayout user={user}>
-      <h2 className="flex justify-center">장바구니</h2>
+      <div>
+        <h2 className="flex justify-center text-red-100"
+          style={{
+            backgroundImage: "url('/images/Bg.png')",
+            backgroundPosition: "center calc(10% - 620px)",
+            backgroundRepeat: "repeat",
+            backgroundSize: "cover",
+            textAlign: "center",
+            minHeight: "200px",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+          }}>장바구니</h2>
+      </div>
       <CartsDetailContent />
     </MainLayout>
   )
