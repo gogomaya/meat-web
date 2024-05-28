632ed084b1db16f2c8e1e6e7830f45f36d54ecc8
gogomaya <ssdd03580@gmail.com>
2024-05-28 14:43:29 +0900

    Add Order-redesign publish

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -9,6 +9,20 @@ const Order = async (props: {
   const {user} = await loginCheck(false)
   return (
     <MainLayout user={user}>
+      <div>
+        <h2 className="flex justify-center text-red-100"
+          style={{
+            backgroundImage: "url('/images/Bg.png')",
+            backgroundPosition: "center calc(10% - 620px)",
+            backgroundRepeat: "repeat",
+            backgroundSize: "cover",
+            textAlign: "center",
+            minHeight: "200px",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+          }}>주문서</h2>
+      </div>
       <OrderDetailContent orderProducts={orderProducts} />
     </MainLayout>
   )
