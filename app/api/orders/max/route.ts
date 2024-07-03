bcf550a6de0cf33d70c9af3a8d4c253f9eb4821e
wwwalohacampus <h850415@naver.com>
2024-07-04 01:15:13 +0900

    order_pk -> order_items 에 들어가지 않는 이슈

--- app/api/orders/max/route.ts
+++ app/api/orders/max/route.ts
@@ -10,7 +10,8 @@ export const GET = async (
 
   const mysql = await mysql2Pool()
   // 마지막으로 삽입된 레코드의 AUTO_INCREMENT 값 가져오기
-  const [pk]: [RowDataPacket[], FieldPacket[]] = await mysql.execute("SELECT LAST_INSERT_ID() order_pk FROM dual")
+  // const [pk]: [RowDataPacket[], FieldPacket[]] = await mysql.execute("SELECT LAST_INSERT_ID() order_pk FROM dual")
+  const [pk]: [RowDataPacket[], FieldPacket[]] = await mysql.execute("SELECT MAX(order_pk) order_pk FROM orders")
   const order_pk = pk[0]["order_pk"]
   // console.log(`등록된 주문번호 - order_pk : ${order_pk}`)
 
