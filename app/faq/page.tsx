943fabe2fbec4554b794890386737e270591ae07
gogomaya <ssdd03580@gmail.com>
2024-07-23 12:40:24 +0900

    Delete redundant faq

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,13 +1,12 @@
 import {loginCheck} from "@/app/users/login/loginCheck"
 import MainLayout from "@/app/main-layout"
-import FAQPage, {FaqSection} from "./faq"
+import {FaqSection} from "./faq"
 
 
 const FAQ = async () => {
   const {user} = await loginCheck(false)
   return (
     <MainLayout user={user}>
-      {/* <FAQPage /> */}
       <FaqSection />
     </MainLayout>
   )
